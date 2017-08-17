6154b26ebbc08bb08a8d85bb3c89a7cfe4d7d6b8
dirtytoeknee <anthony.shaw212@gmail.com>
2017-08-17 17:22:34 -0400

    Kahan Sum complete

--- src/kahan/test/demo.go
+++ src/kahan/test/demo.go
@@ -3,22 +3,35 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"kahan"
+	"math"
+
+	"github.com/dirtytoeknee/na/src/kahan"
 )
 
-const SeriesSize = 100000000
+const SeriesSize = 100000
 
 func main() {
-	nums := make([]float64, SeriesSize)
-	perm := rand.Perm(len(nums))
-	for i := range nums {
-		nums[perm[i]] = float64(i) / 13
+	rnums := make([]float64, SeriesSize)
+	perm := rand.Perm(len(rnums)) // generate random numbers
+
+	for i := range rnums {
+		rnums[perm[i]] = float64(i) / 13
 	}
-	fmt.Printf("Naive sum: %.10f\n", naiveSum(nums))
-	fmt.Printf("Kahan sum: %.10f\n", kahan.RollSum64(nums))
+
+	// fmt.Println(nums)
+
+	n := SumNaive(rnums)
+	k := kahan.SumAll(rnums)
+
+	fmt.Printf("Naive sum: %.10f\n", n)
+	fmt.Printf("Kahan sum: %.10f\n", k)
+
+	diff := math.Abs((n-k))
+	fmt.Printf("Difference: %.10f\n", diff)
+
 }
 
-func naiveSum(nums []float64) float64 {
+func SumNaive(nums []float64) float64 {
 	var sum float64
 	for _, num := range nums {
 		sum += num
